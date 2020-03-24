0292965a5fe8f620afd2bc431af1ece1fefa2fe3
Dylan <dylandecloedt424@gmail.com>
2020-03-24 16:24:11 +0100

    Update diddit.user.js

--- diddit.user.js
+++ diddit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Diddit Taken Volledig
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      2.0
 // @description  Geen zin om taken te maken? Veel plezier xx
 // @updateURL    https://raw.githubusercontent.com/DylanDec/diddit-complete/master/diddit.js
 // @downloadURL  https://raw.githubusercontent.com/DylanDec/diddit-complete/master/diddit.js
@@ -10,20 +10,17 @@
 // @grant        https://www.diddit.be/*
 // ==/UserScript==
 
-
-
 window.addEventListener('keyup', function (e) {
   if (e.keyCode === 38) {
-     alert("Alle opdrachten correct maken?");
-const elements = Array.from(document.querySelectorAll(".ng-binding.ng-scope"));
+     alert("Alle opdrachten correct maken?");  // Aanduide dat opdrachten zullen updaten
+const elements = Array.from(document.querySelectorAll(".ng-binding.ng-scope"));  // Zoeken voor onbegonnen taken
 elements.forEach(element => {
-  element.classList.remove("ng-binding");
-  element.classList.add("exercise-achieved");
+  element.classList.add("exercise-achieved"); // Taak correct
 });
-const elements2 = Array.from(document.querySelectorAll(".ng-scope.exercise-started"));
+const elements2 = Array.from(document.querySelectorAll(".ng-scope.exercise-started")); // Zoeken voor begonnen taken
 elements2.forEach(element2 => {
-  element2.classList.remove("exercise-started");
+  element2.classList.remove("exercise-started"); // Taak correct
 });
-      alert("Alle opdrachten zijn nu correct");
+      alert("Alle opdrachten zijn nu correct"); // Alert dat alles correct is
   }
 }, false);
