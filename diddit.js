3c61b0ed7271441e45b56d5ead22b40e0e08cbcf
Dylan <dylandecloedt424@gmail.com>
2020-03-24 16:04:40 +0100

    Add files via upload

---
+++ diddit.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Diddit Taken Volledig
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Geen zin om taken te maken? Veel plezier xx
+// @author       krijg corona
+// @match        https://www.diddit.be/*
+// @grant        https://www.diddit.be/*
+// ==/UserScript==
+
+
+
+window.addEventListener('keyup', function (e) {
+  if (e.keyCode === 38) {
+     alert("Alle opdrachten correct maken?");
+const elements = Array.from(document.querySelectorAll(".ng-binding.ng-scope"));
+elements.forEach(element => {
+  element.classList.remove("ng-binding");
+  element.classList.add("exercise-achieved");
+});
+const elements2 = Array.from(document.querySelectorAll(".ng-scope.exercise-started"));
+elements2.forEach(element2 => {
+  element2.classList.remove("exercise-started");
+});
+      alert("Alle opdrachten zijn nu correct");
+  }
+}, false);
